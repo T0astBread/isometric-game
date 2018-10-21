805e0a0429edb7439c8610eb4a805f9bc7bdda8a
T0astBread <t0astbreaaad@gmail.com>
2018-10-21 11:37:24 +0200

    Implemented rudimentary player movement
    Note that this movement does not take the map into account and is not very accurate (to the tile grid)

---
+++ Assets/Scripts/MoveBehaviour.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBehaviour : StateMachineBehaviour
+{
+	private const int NORTH_EAST = 0, SOUTH_EAST = 1, SOUTH_WEST = 2, NORTH_WEST = 3;
+
+	public float stepSizeX = 1.575f;
+	public float stepSizeY = .825f;
+	public float jumpHeight = .5f;
+
+	public Sprite[] directionalSprites;  // north east, south east, south west, north west
+
+	private Vector2 movement;
+	private float prevNormalizedTime;
+
+	// OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+	{
+		this.prevNormalizedTime = 0;
+
+		int sprite;
+		
+		float yMovement = GetAxisMovement(animator, "y");
+		float xMovement = 0;
+		if (yMovement == 0)
+		{
+			xMovement = GetAxisMovement(animator, "x");
+			this.movement = new Vector2(this.stepSizeX, -this.stepSizeY);
+			sprite = SOUTH_EAST;
+			if (xMovement < 0)
+			{
+				this.movement *= -1f;
+				sprite = NORTH_WEST;
+			}
+		}
+		else
+		{
+			this.movement = new Vector2(this.stepSizeX, this.stepSizeY);
+			sprite = NORTH_EAST;
+			if (yMovement < 0)
+			{
+				this.movement *= -1f;
+				sprite = SOUTH_WEST;
+			}
+		}
+
+		animator.GetComponent<SpriteRenderer>().sprite = this.directionalSprites[sprite];
+	}
+
+	private float GetAxisMovement(Animator animator, string axisName)
+	{
+		axisName = "movement_" + axisName;
+		float movement = animator.GetFloat(axisName);
+		if (movement != 0) movement = Mathf.Sign(movement);
+		animator.SetFloat(axisName, 0);
+		return movement;
+	}
+
+	// OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+	//override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
+
+	// OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+	//override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
+
+	// OnStateMove is called right after Animator.OnAnimatorMove(). Code that processes and affects root motion should be implemented here
+	override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+	{
+		float prevJumpY = JumpY(this.prevNormalizedTime);
+		float currJumpY = JumpY(stateInfo.normalizedTime);
+		float deltaJumpY = currJumpY - prevJumpY;
+
+		float deltaTime = stateInfo.normalizedTime - this.prevNormalizedTime;
+		this.prevNormalizedTime = stateInfo.normalizedTime;
+
+		Vector2 scaledMovement = this.movement * deltaTime;
+		Vector3 position = animator.transform.position;
+		animator.transform.position = new Vector3(position.x + scaledMovement.x, position.y + scaledMovement.y + deltaJumpY, position.z);
+	}
+
+	private float JumpY(float t) {
+		return Mathf.Clamp((-Mathf.Pow(4 * t - 2, 2) + 4) / 4, 0f, 1f) * this.jumpHeight;
+	}
+
+	// OnStateIK is called right after Animator.OnAnimatorIK(). Code that sets up animation IK (inverse kinematics) should be implemented here.
+	//override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+	private Animator animator;
+
+	// Use this for initialization
+	void Start()
+	{
+		this.animator = GetComponent<Animator>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		HandleMovement("Horizontal", "movement_x");
+		HandleMovement("Vertical", "movement_y");
+	}
+
+	private void HandleMovement(string axisName, string animatorParameterName)
+	{
+		float movement = Input.GetAxisRaw(axisName);
+		this.animator.SetFloat(animatorParameterName, movement);
+	}
+}
