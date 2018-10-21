ce2efbad58435823c88f5180fec8bfc209ee0afd
T0astBread <t0astbreaaad@gmail.com>
2018-10-21 04:00:55 +0200

    Added Player object w/ sprites and changed generated sprite structure

--- Assets/Sprites/build.ps1
+++ Assets/Sprites/build.ps1
@@ -1,20 +1,19 @@
-# Delete previously generated sprites
-rm .\Generated\SingleFiles\*
-
 # Back the old spritesheet up (in case something goes wrong and you need to roll back)
-rm -r .\Generated\Backup
-mkdir .\Generated\Backup
-mv .\Generated\spritesheet.png .\Generated\Backup\spritesheet.png
-mv .\Generated\spritesheet.json .\Generated\Backup\spritesheet.json
+rm -r .\Backup
+cp -Path .\Generated -Destination .\Backup -Container -Recurse
 
-# Save the Aseprite files as .png
-foreach ($file in ls .\AsepriteFiles\) {
+# Save the Aseprite files that belong to the tiles as .png
+foreach ($file in ls .\AsepriteFiles\Tiles) {
 	$file -match "(.+)\.aseprite"  # Match against a file name regex
 	$base_file_name = $Matches[1]  # Get the file name without extension
 
-	Aseprite.exe -b .\AsepriteFiles\$file --save-as .\Generated\SingleFiles\$base_file_name".png"
+	Aseprite.exe -b .\AsepriteFiles\Tiles\$file --save-as .\Generated\Tiles\$base_file_name".png"
 }
 
-# Build the spritesheet
+# Build the tiles spritesheet
 sleep 2  # To accommodate for Aseprite CLI oddities
-Aseprite.exe -b .\Generated\SingleFiles\* --sheet .\Generated\spritesheet.png --data .\Generated\spritesheet.json
+Aseprite.exe -b .\Generated\Tiles\* --sheet .\Generated\Tiles_spritesheet.png --data .\Generated\Tiles_spritesheet.json
+
+# Save the player sprite as a spritesheet
+sleep 2
+Aseprite.exe -b .\AsepriteFiles\player.aseprite --sheet .\Generated\player_spritesheet.png --data .\Generated\player_spritesheet.json
