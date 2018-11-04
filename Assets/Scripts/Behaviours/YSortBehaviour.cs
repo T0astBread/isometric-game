d01d54c07e848d535d1550b17bebfc67db3b3619
T0astBread <t0astbreaaad@gmail.com>
2018-11-04 21:25:29 +0100

    Added YSortBehaviour
    Now the player and enemies are drawn in order of their y positions (top to bottom)

---
+++ Assets/Scripts/Behaviours/YSortBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YSortBehaviour : MonoBehaviour
+{
+	private const int POSITION_MULTIPLIER = 1000;
+
+	private SpriteRenderer spriteRenderer;
+
+	void Start()
+	{
+		this.spriteRenderer = GetComponent<SpriteRenderer>();
+	}
+
+	void Update()
+	{
+		this.spriteRenderer.sortingOrder = -(int)(transform.position.y * POSITION_MULTIPLIER);
+	}
+}
