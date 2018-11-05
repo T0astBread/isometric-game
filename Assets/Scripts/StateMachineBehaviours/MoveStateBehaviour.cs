c11e61b7735f438f35f9c3e312d5befd2b50e26c
T0astBread <t0astbreaaad@gmail.com>
2018-11-05 13:13:02 +0100

    Improved documentation in MoveStateBehaviour

--- Assets/Scripts/StateMachineBehaviours/MoveStateBehaviour.cs
+++ Assets/Scripts/StateMachineBehaviours/MoveStateBehaviour.cs
@@ -6,23 +6,12 @@ public class MoveStateBehaviour : StateMachineBehaviour
 {
 	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
 	{
-		Vector2Int step = Vector2Int.zero;
-
-		float movement = GetAxisMovement(animator, "x");
-		if (movement == 0)
-		{
-			movement = GetAxisMovement(animator, "y");
-			step = Vector2Int.down;
-		}
-		else
-		{
-			step = Vector2Int.right;
-		}
-		step *= (int)Mathf.Sign(movement);
+		Vector2Int step = GetMapStepForMovement(animator);
 
 		Entity entity = animator.GetComponent<Entity>();
 		Vector2Int mapDestination = entity.mapPosition + step;
 
+		// Get the destination tile, if present
 		Tile destinationTile;
 		int entitiesOnTile = 0;
 		if (Map.Tiles.TryGetValue(mapDestination, out destinationTile))
@@ -30,6 +19,7 @@ public class MoveStateBehaviour : StateMachineBehaviour
 			entitiesOnTile = destinationTile.EntitiesOnTile.Count;
 		}
 
+		// Get the right coroutine to use, if any
 		IEnumerator movementCoroutine = null;
 		if (destinationTile != null && entitiesOnTile < 2)
 		{
@@ -42,20 +32,26 @@ public class MoveStateBehaviour : StateMachineBehaviour
 			}
 			else
 			{
+				// Calculate the entity's own offset in the tile
 				Vector2 ownOffset = (Map.ConvertToWorldPosition(mapDestination) - (Vector2)animator.transform.position) * -entity.offsetDistanceWhenMovingToSide;
+				// Tell every other entity on the tile their new offset based on their desired offset distance
 				destinationTile.EntitiesOnTile.ForEach(opposingEntity => {
 					opposingEntity.offsetInTileWhenMovingToSide = -ownOffset.normalized * opposingEntity.offsetDistanceWhenMovingToSide;
 				});
 				movementCoroutine = entity.StepTo(mapDestination, ownOffset);
 			}
+			// If the destination tile isn't empty we're already jumping to the side
 			entity.isAtSideOfTile = !destinationTileIsEmpty;
 		}
+		// If the player is not at the side of the tile (i.e. the tile is empty), make a blocked step
+		// Blocked steps are forbidden on non-empty tiles due to glitchy behaviour
 		else if (entity is Player && !entity.isAtSideOfTile)
 		{
 			Player player = entity as Player;
 			animator.SetFloat("movement_speed_multiplier", 1 / player.blockedStepTime / 2);
 			movementCoroutine = player.StepBlockedTo(mapDestination);
 		}
+		// If the entity can't make a blocked step, cancel the move state immediately
 		else
 		{
 			animator.SetTrigger("cancel_movement");
@@ -64,6 +60,23 @@ public class MoveStateBehaviour : StateMachineBehaviour
 		{
 			entity.StartCoroutine(movementCoroutine);
 		}
+	}
+
+	private Vector2Int GetMapStepForMovement(Animator animator)
+	{
+		Vector2Int step = Vector2Int.zero;
+		float movement = GetAxisMovement(animator, "x");
+		if (movement == 0)
+		{
+			movement = GetAxisMovement(animator, "y");
+			step = Vector2Int.down;
+		}
+		else
+		{
+			step = Vector2Int.right;
+		}
+		step *= (int)Mathf.Sign(movement);
+		return step;
 	}
 
 	private float GetAxisMovement(Animator animator, string axisName)
